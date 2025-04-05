9523a151528ee1877452855b0454bd6ad066d2da
crlxs <140880473+crlxs@users.noreply.github.com>
2025-04-05 10:36:22 +0200

    Vim options

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,3 +1,5 @@
+-- Installing lazy as per documentation
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -14,15 +16,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
+-- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "//"
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "number"
+require("vim-options")
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -30,22 +26,6 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true},
-
-  -- My configs
-  --
-  -- Tabs
-  vim.cmd("set expandtab"),
-  vim.cmd("set tabstop=2"),
-  vim.cmd("set softtabstop=2"),
-  vim.cmd("set shiftwidth=2")
 })
-
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep' })
